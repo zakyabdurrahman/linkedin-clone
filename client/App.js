700ef23fca60eaee4c6a953724f12f6fa49759d1
zakyabdurrahman <zakyabdurrahman10@gmail.com>
2024-07-24 15:30:16 +0700

    finished login screen

--- client/App.js
+++ client/App.js
@@ -1,36 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, Button} from 'react-native';
-import styles from './utils/styles';
-import logo from './assets/linkedin.jpg'
+import LoginScreen from "./views/LoginScreen";
 
 export default function App() {
   return (
     // halaman login
-    <View style={styles.container}>
-      <View>
-        <Image
-          source={logo}
-          style={styles.linkedinLogo}
-        ></Image>
-        <View style={styles.loginTextContainer}>
-          <Text style={styles.h1Login}>Sign in</Text>
-          <Text style={{marginTop: 5, fontSize: 16}}>Stay updated on your professional world </Text>
-          
-        </View>
-        <View style={styles.formGroupLogin}>
-          <TextInput style={styles.form} placeholder='Email or Phone'></TextInput>
-          <TextInput style={styles.form} placeholder='Password' secureTextEntry={true}></TextInput>
-        </View>
-        
-      </View>
-
-      <View style={{marginTop: 30, paddingHorizontal:20}}>  
-        <Button title='Sign in'></Button>
-      </View>
-      
-      
-      <StatusBar style="auto" />
-    </View>
+    <LoginScreen/>
   );
 }
 
---
+++ client/views/LoginScreen.jsx
@@ -0,0 +1,38 @@
+import { StatusBar } from "expo-status-bar"
+import { Button, Image, Text, TextInput, View } from "react-native"
+import styles from "../utils/styles"
+import logo from "../assets/linkedin.jpg"
+
+export default function LoginScreen() {
+    
+    return (
+        <View style={styles.container}>
+            <View>
+                <Image
+                    source={logo}
+                    style={styles.linkedinLogo}
+                ></Image>
+                <View style={styles.loginTextContainer}>
+                    <Text style={styles.h1Login}>Sign in</Text>
+                    <Text style={{marginTop: 5, fontSize: 16}}>Stay updated on your professional world </Text>
+                    
+                </View>
+                <View style={styles.formGroupLogin}>
+                    <TextInput style={styles.form} placeholder='Email or Phone'></TextInput>
+                    <TextInput style={styles.form} placeholder='Password' secureTextEntry={true}></TextInput>
+                </View>
+                
+            </View>
+
+            <View style={{marginTop: 30, paddingHorizontal:20}}>  
+                <Button title='Sign in'></Button>
+                <Text style={{textAlign:"center", marginTop:20}}> New to LinkedIn? <Text style={{color: "blue", fontWeight: "bold"}}>Join now</Text> </Text>
+            </View>
+
+            
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
+
